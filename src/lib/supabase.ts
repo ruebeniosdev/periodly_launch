1570584fa68cc601fc048bf2302b687f73c7056f
ruebeniosdev <akankobatengr@gmail.com>
2025-06-28 01:02:40 +0000

    fix: remove pocket base to use supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,9 +2,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://iqotkrnhkjwrckwsteto.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY;
-if (!supabaseKey) {
-  throw new Error('SUPABASE_KEY environment variable is not set');
-}
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
